e10d53ad3fd63b35b8ad0dab68326ee87673b0e1
Sharapov Kirill <kirillking01@yandex.ru>
2026-04-08 06:09:49 +0300

    add: Инициализировать проект с минимальными зависимостями

---
+++ src/main/java/ru/diploma/studtrack/StudtrackApplication.java
@@ -0,0 +1,13 @@
+package ru.diploma.studtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudtrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/diploma/studtrack/StudtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.diploma.studtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
