d2a71d9751f117be71d14113013ff670c1050990
Sharapov Kirill <kirillking01@yandex.ru>
2026-05-19 15:19:01 +0300

    fix: исправить presigned ссылки для скачивания файлов

--- src/main/java/ru/diploma/studtrack/config/StorageConfig.java
+++ src/main/java/ru/diploma/studtrack/config/StorageConfig.java
@@ -7,6 +7,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 import java.net.URI;
@@ -29,12 +30,16 @@ public class StorageConfig {
 
     @Bean
     public S3Presigner s3Presigner(MinioProperties properties) {
+        String presignEndpoint = properties.publicEndpoint() != null && !properties.publicEndpoint().isBlank()
+                ? properties.publicEndpoint()
+                : properties.endpoint();
         return S3Presigner.builder()
-                .endpointOverride(URI.create(properties.endpoint()))
+                .endpointOverride(URI.create(presignEndpoint))
                 .region(Region.US_EAST_1)
                 .credentialsProvider(StaticCredentialsProvider.create(
                         AwsBasicCredentials.create(properties.accessKey(), properties.secretKey())
                 ))
+                .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
                 .build();
     }
 }
